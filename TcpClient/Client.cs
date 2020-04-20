144e091b4d91e4d72cd43fca2d9cf7ee7a4dbe33
Michael Mayrhofer <michael.mayrhofer@tmconnected.com>
2020-04-20 06:38:57 +0200

    close tcp connection in a correct way

--- TcpClient/Client.cs
+++ TcpClient/Client.cs
@@ -7,6 +7,8 @@ namespace TcpClient
 {
     public class Client : ReceiveActor
     {
+        private IActorRef connectionActor;
+
         public Client()
         {
             Receive<StartMessage>(_ => HandleStart());
@@ -17,30 +19,20 @@ namespace TcpClient
 
         private void HandleStart()
         {
+            Console.WriteLine("tcp.connecting");
             Context.System.Tcp().Tell(new Tcp.Connect(new IPEndPoint(IPAddress.Loopback, 25603)));
         }
 
         private void HandleStop()
         {
             // Closes the TCP connection...
-            Context.System.Tcp().Tell(Tcp.Close.Instance);
-            // ...but causes also the following error message
-            // [ERROR][29.04.2019 05:17:24][Thread 0004][akka://Akka-TcpDemo/system/IO-TCP] The
-            //  supplied message type is invalid. Only Connect and Bind messages are supported.
-            // 
-            // Parametername: message
-            // Cause: System.ArgumentException: The supplied message type is invalid. Only Conn
-            // ect and Bind messages are supported.
-            // Parametername: message
-            //    bei Akka.IO.TcpManager.Receive(Object message)
-            //    bei Akka.Actor.ActorBase.AroundReceive(Receive receive, Object message)
-            //    bei Akka.Actor.ActorCell.ReceiveMessage(Object message)
-            //    bei Akka.Actor.ActorCell.Invoke(Envelope envelope)
+            connectionActor?.Tell(Tcp.Close.Instance);
         }
 
         private void HandleConnected(Tcp.Connected m)
         {
             Console.WriteLine("tcp.connected");
+            connectionActor = Sender;
             Sender.Tell(new Tcp.Register(Self));
         }
 
